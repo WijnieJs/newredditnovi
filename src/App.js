dac90fec4ed63cbd90689a495aaa54f3d8532d09
wijnandK <korlaarwijnand@hotmail.com>
2022-05-31 17:35:41 +0200

    add cssvariables and setheader with routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import Header from './components/header/Header';
+import Home from './pages/home/Home';
+import Subreddit from './pages/subreddit/Subreddit';
+import { Route, Routes } from 'react-router-dom';
+
+import './App.css';
 
 
 
 
 const App = () => {
   return (
-    <div>
+    <div className='testtemplate'>
       <Header />
+      <main>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/subreddit' element={<Subreddit />} />
+
+        </Routes>
+
+      </main>
     </div>
   );
 }
---
+++ src/pages/subreddit/Subreddit.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Subreddit.css';
+
+const Subreddit = () => {
+  return (
+    <div>
+      <h5 className="subreddit-wrapper">Subreddit</h5>
+    </div>
+  );
+}
+
+export default Subreddit;
